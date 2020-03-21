709a02db4c1c52460882a936123a4ca263a6633d
Jotas <jotasimp32@gmail.com>
2020-03-21 23:39:21 +0100

    Clean coordinator.

--- TuVecinoTeAyuda/Classes/ViewControllers/Coordinator.swift
+++ TuVecinoTeAyuda/Classes/ViewControllers/Coordinator.swift
@@ -7,24 +7,36 @@
 
 import UIKit
 
-class Coordinator {
+final class Coordinator {
     
+    // MARK: - Private properties
+    
+    /// Navigation controller responsible of handling all app internal navigation
     private let navigationController: UINavigationController
+    
+    // MARK: - Object lifecycle
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
+    // MARK: - Internal methods
+    
+    /// Starts the navigation
     func start() {
         showLoginViewController()
     }
     
+    // MARK: - Private methods
+    
+    /// Create and show login view controller
     private func showLoginViewController() {
         let loginViewController = LoginViewController()
         loginViewController.delegate = self
         navigationController.setViewControllers([loginViewController], animated: false)
     }
     
+    /// Create and show volunteer registration view controller
     private func showVolunteerRegister() {
 //        let volunteerRegisterViewController = VolunteerRegisterViewController()
 //        volunteerRegisterViewController.delegate = self
@@ -32,6 +44,7 @@ class Coordinator {
         debugPrint("showVolunteerRegister")
     }
     
+    /// Create and show requestor registration view controller
     private func showRequestorRegister() {
     //        let requestorRegisterViewController = RequestorRegisterViewController()
     //        requestorRegisterViewController.delegate = self
@@ -40,7 +53,9 @@ class Coordinator {
         }
 }
 
-// MARK: - LoginViewControllerDelegate
+// MARK: - Extensions
+
+// MARK: LoginViewControllerDelegate
 
 extension Coordinator: LoginViewControllerDelegate {
     func loginViewControllerRegisterVolunteer(_ sender: LoginViewController) {
