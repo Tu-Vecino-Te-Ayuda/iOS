57d61b9ce386bc7c494c4976254af60ee8efde7f
Jotas <jotasimp32@gmail.com>
2020-03-28 13:17:07 +0100

    Rebasing.

--- TuVecinoTeAyuda/Classes/Common/Constants.swift
+++ TuVecinoTeAyuda/Classes/Common/Constants.swift
@@ -10,5 +10,6 @@ import UIKit
 struct Constants {
     enum Colors {
         static let main = UIColor(red: 255 / 255, green: 193 / 255, blue: 7 / 255, alpha: 1) // Hex: #FFC107
+        static let green: UIColor = .init(red: 107 / 255, green: 209 / 255, blue: 189 / 255, alpha: 1) // Hex: #6BD1BD
     }
 }
