70468808ff1dd05e63306e0c507918976261da4f
Jotas <jotasimp32@gmail.com>
2020-04-01 00:57:47 +0200

    Add darker color variants.

--- TuVecinoTeAyuda/Classes/Common/Constants.swift
+++ TuVecinoTeAyuda/Classes/Common/Constants.swift
@@ -9,7 +9,9 @@ import UIKit
 
 struct Constants {
     enum Colors {
-        static let main = UIColor(red: 255 / 255, green: 193 / 255, blue: 7 / 255, alpha: 1) // Hex: #FFC107
-        static let green: UIColor = .init(red: 107 / 255, green: 209 / 255, blue: 189 / 255, alpha: 1) // Hex: #6BD1BD
+        static let main = UIColor(red: 255 / 255, green: 193 / 255, blue: 7 / 255, alpha: 1) // Hex: #FFC107 Amber
+        static let darkMain = UIColor(red: 245 / 255, green: 183 / 255, blue: 0 / 255, alpha: 1) // Hex: #f5b700
+        static let green = UIColor(red: 107 / 255, green: 209 / 255, blue: 189 / 255, alpha: 1) // Hex: #6BD1BD Downy
+        static let darkGreen = UIColor(red: 97 / 255, green: 199 / 255, blue: 179 / 255, alpha: 1) // Hex: #61C7B3
     }
 }
